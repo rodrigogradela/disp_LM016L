998502a4c6c68217ddce563669895349a2d671b4
rodrigogradela <57441535+rodrigogradela@users.noreply.github.com>
2020-06-13 16:41:18 -0300

    Update main.asm

--- assembly/display/main.asm
+++ assembly/display/main.asm
@@ -11,34 +11,33 @@
 
 
 .include "m32def.inc"
-.equ en=PB3
-.equ rs=pb2
-.equ rw=PB1
+.equ en=PB3 ;configurações iniciais para o display (necessarias)
+.equ rs=pb2;configurações iniciais para o display (necessarias)
+.equ rw=PB1;configurações iniciais para o display (necessarias)
 .org 0x00
 rjmp strt
 
 
-strt:
-
-cli
-ldi r16, high(ramend)
-out sph, r16
-ldi r16, low(ramend)
-out spl, r16
+strt: ; função de inicio(start)
+cli ; desativa interrupção global
+ldi r16, high(ramend); ultima localização da memoria no chip(ramend)
+out sph, r16;saida
+ldi r16, low(ramend); ultima localização da memoria no chip(ramend)
+out spl, r16 ;saida
 
 ldi r16, 0xff
-out ddrb,r16
-out ddrc, r16
+out ddrb,r16 ;saida
+out ddrc, r16;saida
 
 ldi r16, 0x00
 out portb, r16
 out portc, r16
 
-ldi r17,0x80  ;Carrega 80h no registrador auxiliar 16
+	ldi r17,0x80  ;Carrega 80h no registrador auxiliar 16
 	out DDRD,r17  ;Configurando o PORTD7 como saida
 	ldi r17,0x40  ;Carregando 40h no registrador auxiliar 16
 	out PORTD,r17 ;Inicializando P07 em LOW e habilito P06 como pull-up
-  call LCDinit
+ 	 call LCDinit
 
  
  
@@ -53,18 +52,18 @@ ldi r17,0x80  ;Carrega 80h no registrador auxiliar 16
 	sbi PORTD,PD7   ; Liga led
 	rjmp msg_ligado ; Muda mensagem para ligado
 	
-    msg_ligado:
+    	msg_ligado:
 	 call LCDinit
-call dsplystr
- ldi zh,high(mesg2<<1)	; Carrega imediatamente do zh para o high
- ldi zl,low (mesg2<<1)	; Carrega imediatamente do zh para o low
- rjmp loop
+	call dsplystr ; chama o display
+ 	ldi zh,high(mesg2<<1)	; Carrega imediatamente do zh para o high
+ 	ldi zl,low (mesg2<<1)	; Carrega imediatamente do zh para o low
+ 	rjmp loop
 
 
  con:
  rjmp con
- LCDinit:
-
+ 
+ LCDinit: ; inicialização do display
  ldi r16, 0x01	; Carregando o registrador r16
  call cmndwrt	;  Chamando a função cmndwrt do LCD
  ldi r16, 0x06	; Carregando o registrador r16 em 0x06
@@ -78,54 +77,54 @@ call dsplystr
  call dsplystr	; Chama a função do display
  ldi zh,high(mesg<<1)	; Carrega zh para high
  ldi zl,low (mesg<<1)	; Carrega zh para low
- ret
+ ret ; garante o retorno da subrotina
  dsplystr:
- clr r0
- cp r16, r0
+ clr r0 ; limpa o registro
+ cp r16, r0 ;compara os registros
  lpm r16,z+	; Carrega a memoria r16 do z
- breq check
+ breq check ; faz um branch se igual
  call datawrt
  rjmp dsplystr
  
 
  check:
- ret
+ ret ; garante o retorno da subrotina
  cmndwrt:
- cbi portb, en	;Limpa registrador com a porta en
- cbi portb, rs	;Limpa registrador com a porta rs
+ cbi portb, en	;Limpa registrador com a porta en requisito obrigatorio para o uso do display
+ cbi portb, rs	;Limpa registrador com a porta rs requisito obrigatorio para o uso do display
  cbi portb, rw
  out portc,r16	;Configura registrador r16 como saida da porta C
- sbi portb, en
+ sbi portb, en ;defini o bit no registro i/o
  call delay
  cbi portb, en
- ret
- datawrt:
- cbi portb, en
- sbi portb, rs
+ ret; garante o retorno da subrotina
+ datawrt: ;configuração da datawart do display
+ cbi portb, en ;limpa bit no registro i/o
+ sbi portb, rs ;defini o bit no registro i/o
  cbi portb, rw
  out portc,r16
- sbi portb, en
+ sbi portb, en ;defini o bit no registro i/o
  call delay
  cbi portb, en
- ret
+ ret; garante o retorno da subrotina
 
- delay:             ;delay de 2 ms
- push r16
- push r17
+ delay:             ;carrega toda instrução para um delay de 2 ms 
+ push r16 ; empurra o r16 para pilha
+ push r17 ; empurra o r16 para pilha
  ldi r16, 0xff
  a1:ldi r17, 0x3f
  a2: dec r17
- brne a2
- dec r16
- brne a1
- pop r16
- pop r17
+ brne a2 ;faz branch se nao for igual
+ dec r16 ;decrementa r16
+ brne a1 ;faz branch se nao for igual
+ pop r16 ; pega registro da pilha
+ pop r17 ; pega o registro da pilha
  ret
 
  
 
- mesg: .db "Fan Desligada!!!!",0x00
-  mesg2: .db "Fan Ligada!!!! ",0x00
+ mesg: .db "Fan Desligada!!!!",0x00 ;mensagem inicial
+  mesg2: .db "Fan Ligada!!!! ",0x00 ; mensagem quando circuito esta ativo
  
 
  
